57a1b762c4b6bd93323a59519626369a160fb7e3
Nic Schumann <nic@nicschumann.co>
2021-03-31 13:37:37 -0400

    [format] removes extra spaces

--- src/stories/spring.js
+++ src/stories/spring.js
@@ -1,15 +1,6 @@
 {
   id: 'spring',
-  text: `I want to start with the weather. *1000
-    The first thing I notice is that I can smell again. *1000
-    A week ago, *350 on my walk, *350 all I could smell was the cold. *1000
-    The ocean was gray under gray clouds; *750 the horizon was gray, *350
-    although it brightened, *350 briefly, *350 towards sundown. *1000
-    The air carried no scent but bit in my nostrils, *350 acrid. *1500
-    Today, I smell everything. *1500
-    Suddenly, *350 the air is dense *150 and living – *500
-    full of information, *350 unfurled flowers, *350 pheromones, *350 and warm skin. *1000
-    Spring is here.`,
+  text: `I want to start with the weather. *1000 The first thing I notice is that I can smell again. *1000 A week ago, *350 on my walk, *350 all I could smell was the cold. *1000 The ocean was gray under gray clouds; *750 the horizon was gray, *350 although it brightened, *350 briefly, *350 towards sundown. *1000 The air carried no scent but bit in my nostrils, *350 acrid. *1500 Today, I smell everything. *1500 Suddenly, *350 the air is dense *150 and living – *500 full of information, *350 unfurled flowers, *350 pheromones, *350 and warm skin. *1000 Spring is here.`,
   marginalia: [
   ],
   sidelines: [
