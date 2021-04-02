15a3e98f8ec8488a8c0bdef1be671c341616bbf2
Nic Schumann <nic@nicschumann.co>
2021-04-02 14:06:26 -0400

    [tangent] student tangents

---
+++ src/stories/students-part-one.js
@@ -0,0 +1,20 @@
+{
+  id: 'students-part-one',
+
+  text: `Actually, that’s not quite true. *1500 The roots are deep, *350 but I can take you there… *500 I have a long crit day ahead with my students. *1500 As I enter the classroom, *350 I am greeted with over-caffeinated eyes *350 still resisting the urge to crash after 12-plus hours of graphic design homework. *1500 The curriculum here is brutal, *350 and the culture is  unhealthy. *1500 Still, *350 I love being here, *350 simply because I can watch the world from inside the classroom *250 through weekly assignments. *1500 Every deliverable is a portal opening up on a slice of culture, *350 on a good day *350 a deliberate critique, *350 on other days *350 a reflection of their desires.`,
+
+  marginalia: [
+  ],
+
+  sidelines: [
+  ],
+
+  font: {
+    size: 36
+  },
+
+  transitions: {
+    next: [{id: 'students-part-two'}],
+    prev: []
+  }
+}
