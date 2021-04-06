773c033cdecfe51c9076cef3ef3c7d830de55970
Nic Schumann <nic@nicschumann.co>
2021-04-06 11:45:51 -0400

    [trailer] adds a cue to scroll up/down

--- src/stories/trailer/our-story.js
+++ src/stories/trailer/our-story.js
@@ -1,6 +1,6 @@
 {
   id: 'our-story',
-  text: `In the story, *350 we follow a character *150 who stumbles upon a mysterious mushroom. *1500 Various events unfold from there. *1500 You, *250 as the reader, *250 have the option to navigate the narrative, *350 unlocking various pockets along the way. *1500 You can control the flow of the story through scrolling downwards or upwards. *1500 On each frame, *350 the selections will take you through different paths. *1500 This means you can reload the page, *350 and follow alternate versions through the story.`,
+  text: `In the story, *350 we follow a character *150 who stumbles upon a mysterious mushroom. *1500 Various events unfold from there. *1500 You, *250 as the reader, *250 have the option to navigate the narrative, *350 unlocking various pockets along the way. *1500 You can control the flow of the story through scrolling downwards or upwards. *1500 On each frame, *350 the selections will take you through different paths. *1500 This means you can reload the page, *350 and follow alternate versions through the story. *1500 You can also scroll up or down on this frame, *350 right now, *450 to take different paths.`,
   marginalia: [
   ],
   sidelines: [
