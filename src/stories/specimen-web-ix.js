c2d0a0aa478c93e8d0dfdce75f9f98355d8eff84
Nic Schumann <nic@nicschumann.co>
2021-04-23 12:12:14 -0400

    [length] minor length and copy-edits.

--- src/stories/specimen-web-ix.js
+++ src/stories/specimen-web-ix.js
@@ -1,8 +1,7 @@
 {
   id: 'specimen-web-ix',
 
-  text: `Mantar#h#reg Regular#h#reg$break We hope this “specimen” has showcased the potential of the world Mantar, both as a typeface and as a point of view. If you have any questions or comments, we’d love to have a conversation. Feel&nbsp;free&nbsp;to&nbsp;reach&nbsp;out.$lcontact`,
-  // link to : https://occupantfonts.com/about#contact
+  text: `Mantar#h#reg Regular#h#reg$break We hope this “specimen” has showcased the potential of the world of Mantar, both as a typeface and as a point of view. If you have any questions or comments, we’d love to have a conversation. Feel&nbsp;free&nbsp;to&nbsp;reach&nbsp;out.$lcontact`,
 
   marginalia: [
   ],
--- src/stories/specimen-web-viii.js
+++ src/stories/specimen-web-viii.js
@@ -1,7 +1,8 @@
 {
   id: 'specimen-web-viii',
 
-  text: `Mantar#h#reg Regular#h#reg$break This became the framework for the non-linear narrative which explores the themes embedded in the development of Mantar. *2500 The creation of this narrative was heavily influenced by the tools we built, *550 while the tools evolved to meet the needs of the narrative. *1500 This interlocking creative process is an important facet to both of our practices, *550 and the practice of Occupant Fonts as a foundry.`,
+  text: `Mantar#h#reg Regular#h#reg$break This became the framework for the non-linear narrative which explores the themes embedded in the development of Mantar. *2500 The creation of this narrative was heavily influenced by the tools we built, *550 while the tools evolved to meet the needs of the narrative.`,
+  // *1500 This interlocking creative process is an important facet to both of our practices, *550 and the practice of Occupant Fonts as a foundry.
 
   marginalia: [
   ],
