1455a36c89f4b76f85bba0059b7614ef90f55c5e
Nic Schumann <nic@nicschumann.co>
2021-10-17 16:32:06 -0400

    [scripts] remove scripts, update glyphs-ii

--- src/stories/glyphs-ii.js
+++ src/stories/glyphs-ii.js
@@ -1,6 +1,6 @@
 {
   id: 'glyphs-ii',
-  text: `I start laying down the nodes *750 and pull the curves by their handles. *1500 Segments cut the light, *400 defining the dark. *1500 Contours *200 emerge, *500 letter by letter. *1500 I space the glyphs whose job is to control the alphabet; *1000 n, *150 o, *150 p *500 which become *250 b, *250 d, *175 q, *175 m, *150 h, *150 r, *150 l, *150 i, *150 f, *150 j, *100 c, *100 &$trim c$trim .`,
+  text: `I start laying#r down the nodes *750 and pull the curves by their handles. *1500 Segments cut the light, *400 defining the dark. *1500 Contours *200 emerge, *500 letter by letter. *1500 I space the glyphs whose job is to control the alphabet; *1000 n, *150 o, *150 p *500 which become *250 b, *250 d, *175 q, *175 m, *150 h, *150 r, *150 l, *150 i, *150 f, *150 j, *100 c, *100 &$trim c$trim .`,
   marginalia: [
   ],
   sidelines: [
